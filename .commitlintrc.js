5f0c766de2b4ac33a07485987f7ea79f208ed6ed
onelong <love@onelong.org>
2019-01-04 19:35:41 +0800

    revert(packages): eslint v4.19.1

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -9,6 +9,6 @@ module.exports = {
     'type-empty': [2, 'never'],
     'type-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'release']]
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'release','revert']]
   }
 }
