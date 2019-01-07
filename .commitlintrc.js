3c17acbe2b67acad6471f273130bd8afab22c688
onelong <love@onelong.org>
2019-01-07 14:23:28 +0800

    chore(commitlintrc): allow `prefer: some changes`

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -9,6 +9,10 @@ module.exports = {
     'type-empty': [2, 'never'],
     'type-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'release','revert']]
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'release', 'revert', 'prefer']
+    ]
   }
 }
